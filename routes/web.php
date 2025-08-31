bcbb45af0763c1c65376e70f84a3d0e21a8ff352
eduardoranucci <eduardoranucci@outlook.com>
2025-08-30 22:13:53 -0300

    refactor: adjust routes to use controllers for view rendering

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
-
-Route::get('/privacy-policy', function () {
-    return view('privacy-policy');
-})->name('privacy-policy');
+Route::get('/', [HomeController::class, 'welcome'])->name('welcome');
+Route::get('/privacy-policy', [HomeController::class, 'privacyPolicy'])->name('privacy-policy');
 
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/home', function () {
-        return view('home');
-    })->name('home');
-
-    Route::get('/user/{user}', function (User $user) {
-        $view = 'auth.update-profile';
-        if ($user->role === 'teacher') {
-            $view = 'auth.update-profile-teacher';
-        } elseif ($user->role === 'student') {
-            $view = 'auth.update-profile-student';
-        } elseif ($user->role === 'patient') {
-            $view = 'auth.update-profile-patient';
-        }
-        return view($view, ['user' => $user]);
-    })->name('user.edit');
-
-    Route::get('/user/password/{user}', function (User $user) {
-        return view('auth.update-password', ['user' => $user]);
-    })->name('user.password-edit');
+    Route::get('/home', [HomeController::class, 'home'])->name('home');
+    Route::get('/user/{user}', [UserController::class, 'edit'])->name('user.edit');
+    Route::get('/user/password/{user}', [UserController::class, 'editPassword'])->name('user.password-edit');
 
     // Exemplos de rotas com middleware de validação de tipo de usuário
 
